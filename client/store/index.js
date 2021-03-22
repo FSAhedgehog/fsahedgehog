8f72311d8eb7013fc6f2e517d34b6a356a9b214a
Stone Fuglaar <fuglaarusa@gmail.com>
2021-03-22 14:09:00 -0700

    Feature: single hedgefund reducer

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import singleHedgefund from './singleHedgefund'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, singleHedgefund})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
