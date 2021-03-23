f11bf9f803274d1572cd07e5ee9ecea952558a83
Edward Ledbetter <edledbetter@icloud.com>
2021-03-22 20:06:43 -0400

    user-route-fixed

--- server/api/users.js
+++ server/api/users.js
@@ -3,29 +3,15 @@ const {User, Stock} = require('../db/models')
 const Sequelize = require('sequelize')
 module.exports = router
 
-// router.get('/', async (req, res, next) => {
-//   try {
-//     const users = await User.findAll({
-//       // explicitly select only the id and email fields - even though
-//       // users' passwords are encrypted, it won't help if we just
-//       // send everything to anyone who asks!
-//       attributes: ['id', 'email']
-//     })
-//     res.json(users)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
 router.get('/', async (req, res, next) => {
   try {
-    const stockValue = await Stock.findAll({
-      where: {
-        ticker: '',
-        thirteenFId: 1,
-      },
+    const users = await User.findAll({
+      // explicitly select only the id and email fields - even though
+      // users' passwords are encrypted, it won't help if we just
+      // send everything to anyone who asks!
+      attributes: ['id', 'email'],
     })
-    res.json(stockValue[0].totalValue)
+    res.json(users)
   } catch (err) {
     next(err)
   }
