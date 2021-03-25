934878281c3bc094c0ef517ec3fe5f81f40628a8
Logan Sutherland <loganrsutherland@gmail.com>
2021-03-25 18:47:35 -0400

    fix: sorting 13Fs now works

---
+++ script/calculateSP.js
@@ -0,0 +1,34 @@
+const {ThirteenF, HedgeFund} = require('../server/db/models')
+const {getPrice} = require('./seederUtility')
+
+const STARTING_VALUE = 10000
+
+async function calculateSPValue() {
+  const hedgeFunds = await HedgeFund.findAll({
+    include: ThirteenF,
+    order: [[ThirteenF, 'dateOfFiling', 'ASC']],
+  })
+
+  console.log(hedgeFunds[0].thirteenFs)
+
+  hedgeFunds.forEach(async (hedgeFund) => {
+    const thirteenFs = hedgeFund.thirteenFs
+    const first13F = thirteenFs[0]
+    first13F.spValue = Math.round(STARTING_VALUE)
+    await first13F.save()
+    let initialPrice = await getPrice('^GSPC', first13F.dateOfFiling)
+    initialPrice = initialPrice[0].close
+    console.log('INITIAL PRICE——————', initialPrice)
+    const startingShares = STARTING_VALUE / initialPrice
+
+    for (let i = 1; i < thirteenFs.length; i++) {
+      const current13F = thirteenFs[i]
+      const currentPrice = await getPrice('^GSPC', current13F.dateOfFiling)
+      const currentValue = Math.round(startingShares * currentPrice)
+      current13F.spValue = currentValue
+      await current13F.save()
+    }
+  })
+}
+
+calculateSPValue()
