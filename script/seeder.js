bc0d98d5be1fe741d4fc5fe2caba92230c010d37
Logan Sutherland <loganrsutherland@gmail.com>
2021-03-25 19:08:36 -0400

    fix: add try/catch to spValue func

--- script/seeder.js
+++ script/seeder.js
@@ -311,33 +311,37 @@ async function setMimicReturn(year, quarter) {
 }
 
 async function calculateSPValue() {
-  const hedgeFunds = await HedgeFund.findAll({
-    include: ThirteenF,
-    order: [[ThirteenF, 'dateOfFiling', 'ASC']],
-  })
+  try {
+    const hedgeFunds = await HedgeFund.findAll({
+      include: ThirteenF,
+      order: [[ThirteenF, 'dateOfFiling', 'ASC']],
+    })
 
-  console.log(hedgeFunds[0].thirteenFs)
+    console.log(hedgeFunds[0].thirteenFs)
 
-  hedgeFunds.forEach(async (hedgeFund) => {
-    const thirteenFs = hedgeFund.thirteenFs
-    const first13F = thirteenFs[0]
-    first13F.spValue = Math.round(STARTING_VALUE)
-    await first13F.save()
-    let initialPrice = await getPrice('^GSPC', first13F.dateOfFiling)
-    initialPrice = initialPrice[0].close
-    console.log('INITIAL PRICE——————', initialPrice)
-    const startingShares = STARTING_VALUE / initialPrice
+    hedgeFunds.forEach(async (hedgeFund) => {
+      const thirteenFs = hedgeFund.thirteenFs
+      const first13F = thirteenFs[0]
+      first13F.spValue = Math.round(STARTING_VALUE)
+      await first13F.save()
+      let initialPrice = await getPrice('^GSPC', first13F.dateOfFiling)
+      initialPrice = initialPrice[0].close
+      console.log('INITIAL PRICE——————', initialPrice)
+      const startingShares = STARTING_VALUE / initialPrice
 
-    for (let i = 1; i < thirteenFs.length; i++) {
-      const current13F = thirteenFs[i]
-      console.log('13F ID—————', current13F.id)
-      let currentPrice = await getPrice('^GSPC', current13F.dateOfFiling)
-      currentPrice = currentPrice[0].close
-      const currentValue = Math.round(startingShares * currentPrice)
-      current13F.spValue = currentValue
-      await current13F.save()
-    }
-  })
+      for (let i = 1; i < thirteenFs.length; i++) {
+        const current13F = thirteenFs[i]
+        console.log('13F ID—————', current13F.id)
+        let currentPrice = await getPrice('^GSPC', current13F.dateOfFiling)
+        currentPrice = currentPrice[0].close
+        const currentValue = Math.round(startingShares * currentPrice)
+        current13F.spValue = currentValue
+        await current13F.save()
+      }
+    })
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 function calcHedgeFundReturn(years) {
