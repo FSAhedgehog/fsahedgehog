f8a9be7ea8659355c0108f44c1bd126686b82929
Logan Sutherland <loganrsutherland@gmail.com>
2021-03-26 10:47:18 -0400

    feat: fix syntax error

--- script/experiment.js
+++ script/experiment.js
@@ -1,27 +1,25 @@
 const yahooFinance = require('yahoo-finance')
 
-function getBeta(ticker) {
-  return yahooFinance.quote(
-    {
-      symbol: ticker,
-      modules: ['summaryDetail'],
-    },
-    function (err, quotes) {
-      if (err) {
-        // console.log(err)
-      } else {
-        let beta = quotes.summaryDetail.beta
-        // console.log("THIS IS BETA", beta)
-        return beta
-      }
-    }
-  )
+async function getBeta(ticker) {
+  const newTicker = '^GSPC'
+  try {
+    const response = await yahooFinance.quote(
+      {
+        symbol: ticker,
+        modules: ['summaryDetail'],
+      },
+      {symbol: newTicker, modules: ['summaryDetail']}
+    )
+    return response.summaryDetail.beta
+  } catch (err) {
+    console.error(err)
+  }
 }
+
 async function test() {
   for (let i = 0; i < 1000; i++) {
     const beta = await getBeta('VRSK')
-    console.log(beta.summaryDetail.beta)
-    if (!beta.summaryDetail) break
+    console.log(beta)
   }
 }
 
