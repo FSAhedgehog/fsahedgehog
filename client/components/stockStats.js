250982cf128100928a6c10852c7a9cf415c8d288
Evan-Fisher <evankfisher@gmail.com>
2021-07-04 18:12:17 -0500

    a lot of the stock stats styling. Working on having sticky titles

--- client/components/stockStats.js
+++ client/components/stockStats.js
@@ -6,15 +6,43 @@ export class StockStats extends React.Component {
     let stockStats = this.props.stockStats
     if (!this.props.loading) {
       return (
-        <div className="stock-stats-container">
+        <div id="growth" className="stock-stats-container">
+          <div className="data-labels">
+            <p>Rank</p>
+            <p>Ticker</p>
+            <p>Ownership Count</p>
+            <p>Total % of Portfolios</p>
+            <p>Total $</p>
+          </div>
           <div className="stock-stats-box">
             {stockStats
               .sort((a, b) => b.count - a.count)
-              .map((stock, index) => (
-                <div key={stock.id}>{stock.ticker}</div>
-              ))}
+              .map((stock, index) => {
+                let totalInvested = stock.totalInvested
+                let amntIndicator
+                if (totalInvested > 1000000000) {
+                  totalInvested = totalInvested / 1000000000
+                  totalInvested = round(totalInvested, 1)
+                  amntIndicator = 'B'
+                } else {
+                  totalInvested = totalInvested / 1000000
+                  totalInvested = round(totalInvested, 0)
+                  amntIndicator = 'M'
+                }
+                return (
+                  <div key={stock.id} className="data-container">
+                    <p id="rank">{index + 1}</p>
+                    <p id="ticker">{stock.ticker}</p>
+                    <p id="count">{stock.count}</p>
+                    <p id="percentage">{`${(
+                      stock.totalPercentage * 100
+                    ).toFixed(2)}%`}</p>
+                    <p id="invested">{`$${totalInvested} ${amntIndicator}`}</p>
+                  </div>
+                )
+              })}
           </div>
-          <div>
+          {/* <div>
             {stockStats
               .sort((a, b) => b.totalInvested - a.totalInvested)
               .map((stock) => (
@@ -27,7 +55,7 @@ export class StockStats extends React.Component {
               .map((stock) => (
                 <div key={stock.id}>{stock.totalPercentage}</div>
               ))}
-          </div>
+          </div> */}
         </div>
       )
     } else {
@@ -44,3 +72,8 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps)(StockStats)
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0)
+  return Math.round(value * multiplier) / multiplier
+}
